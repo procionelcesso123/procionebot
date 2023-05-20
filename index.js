9f3e41cfd10399e62d42eceff4be0f391ed11e06
procionelcesso123 <118928642+procionelcesso123@users.noreply.github.com>
2023-05-20 13:50:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('CM_ESOTO', {
+say('ESOTO SPACE', {
 font: 'shade', 
 align: 'center',
 gradient: ['blue', 'white']})
-say('ediz fab & dan', {
+say('ediz procione', {
 font: 'tiny',
 align: 'center',
 gradient: ['red', 'blue']})
