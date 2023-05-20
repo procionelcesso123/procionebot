a641e17d78834cd8160a2aff9dfd7f44185cd484
procionelcesso <118928642+procionelcesso123@users.noreply.github.com>
2023-05-20 19:24:42 +0200

    Update random-procione.js

--- plugins/random-procione.js
+++ plugins/random-procione.js
@@ -1,18 +1,21 @@
-import fetch from 'node-fetch'
-let handler  = async (m, { conn, text }) => {
-try {
-let res = await fetch('procione.jpg')
-let img = await res.buffer()
-let caption = `
-*QUESTO NON E' UN PROCIONE, E' IL PROCIONE, @PROCIONECESSO!*
-`.trim()
-conn.sendFile(m.chat, img, 'procione.jpg', caption, m)
-} catch (e) {
-console.log(e)
-throw '*Error!*'
-}}
-handler.help = ['procione']
-handler.tags = ['random']
-handler.command = 'procione'
-handler.fail = null
-export default handler 
+import os from 'os'
+import util from 'util'
+import sizeFormatter from 'human-readable'
+import MessageType from '@adiwajshing/baileys'
+import fs from 'fs'
+import { performance } from 'perf_hooks'
+let handler = async (m, { conn, usedPrefix }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const used = process.memoryUsage()
+const { restrict } = global.db.data.settings[conn.user.jid] || {}
+const { autoread } = global.opts
+const grup = './procione.jpg'
+let old = performance.now()
+let neww = performance.now()
+let speed = neww - old
+let text = *QUESTO NON E' UN PROCIONE, E' IL PROCIONE, @PROCIONELCESSO!
