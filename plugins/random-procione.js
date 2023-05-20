2b090af7e7c8de501b876c0cab447335b2615f1b
procionelcesso <118928642+procionelcesso123@users.noreply.github.com>
2023-05-20 18:39:47 +0200

    Create random-procione.js

---
+++ plugins/random-procione.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, text }) => {
+try {
+let res = await fetch('procione.jpg')
+let img = await res.buffer()
+let caption = `
+*OH MA QUESTA E UNA TIGRE*
+`.trim()
+conn.sendFile(m.chat, img, 'procione.jpg', caption, m)
+} catch (e) {
+console.log(e)
+throw '*Error!*'
+}}
+handler.help = ['procione']
+handler.tags = ['random']
+handler.command = 'procione'
+handler.fail = null
+export default handler 
