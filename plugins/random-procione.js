b34936f5c1134cf3460f5fd095b7fa7b8b58931e
procionelcesso <118928642+procionelcesso123@users.noreply.github.com>
2023-05-21 16:35:50 +0200

    Update random-procione.js

--- plugins/random-procione.js
+++ plugins/random-procione.js
@@ -1,34 +1,16 @@
-import os from 'os'
-import util from 'util'
-import sizeFormatter from 'human-readable'
-import MessageType from '@adiwajshing/baileys'
-import fs from 'fs'
-import { performance } from 'perf_hooks'
-let handler = async (m, { conn, usedPrefix }) => {
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime) 
-let totalreg = Object.keys(global.db.data.users).length
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
-const groups = chats.filter(([id]) => id.endsWith('@g.us'))
-const used = process.memoryUsage()
-const { restrict } = global.db.data.settings[conn.user.jid] || {}
-const { autoread } = global.opts
-const grup = './procione.jpg'
-let old = performance.now()
-let neww = performance.now()
-let speed = neww - old
-let text = 
-
-*QUESTO NON E' UN PROCIONE, E' IL PROCIONE, @PROCIONELCESSO!*
-`.trim()
-conn.sendFile(m.chat, img, 'procione.jpg', caption, m)
-} catch (e) {
-console.log(e)
-throw '*Error!*'
-}}
-handler.help = ['procione']
-handler.tags = ['random']
-handler.command = 'procione'
-handler.fail = null
-export default handler  
+let handler = async (m, {
+conn, groupMetadata
+}) => {
+if (!m.isGroup)
+throw ''
+let gruppi = global.db.data.chats[m.chat]
+if (gruppi.spacobot === false)
+throw ''
+m.reply(`${toM(a)} ${pickRandom([L'UNICO PROCIONE E' https://www.instagram.com/procionelcesso/])} ${toM(b)}`, null, {
+})}
+handler.customPrefix = /procione/i
+handler.command = new RegExp
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+}
