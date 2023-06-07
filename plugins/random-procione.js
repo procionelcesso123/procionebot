982d605742f832ed5e929b6ed7ae49c8fb037d80
procionelcesso <118928642+procionelcesso123@users.noreply.github.com>
2023-06-07 17:01:39 +0200

    Update random-procione.js

--- plugins/random-procione.js
+++ plugins/random-procione.js
@@ -1,16 +1,1 @@
-let handler = async (m, {
-conn, groupMetadata
-}) => {
-if (!m.isGroup)
-throw ''
-let gruppi = global.db.data.chats[m.chat]
-if (gruppi.spacobot === false)
-throw ''
-m.reply(`${toM(a)} ${pickRandom([L'UNICO PROCIONE E' www.instagram.com/procionelcesso])} ${toM(b)}`, null, {
-})}
-handler.customPrefix = /procione/i
-handler.command = new RegExp
-export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-}
+
