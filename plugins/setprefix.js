2d73d9eabe40a1fa2e8711083cd05e34c1910496
procionelcesso <118928642+procionelcesso123@users.noreply.github.com>
2023-06-10 02:50:13 +0200

    Update setprefix.js

--- plugins/setprefix.js
+++ plugins/setprefix.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `[❗𝐈𝐍𝐅𝐎❗] Hey non hai inserito nessun prefisso...`
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-  await m.reply(`[❗𝐈𝐍𝐅𝐎❗] Il prefisso è stato cambiato in *${text}*`)
+  if (!text) throw `[❗𝐈𝐍𝐅𝐎❗] Deficente non hai inserito nessun prefisso valido...`
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-☆▪︎♡◇♧₩●○🦝').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  await m.reply(`[❗𝐈𝐍𝐅𝐎❗] Hai cambiato il prefisso per i comandi di procione in: *${text}*`)
     // conn.fakeReply(m.chat, '[❗𝐈𝐍𝐅𝐎❗] Il prefisso è stato cambiato in *${text}*', '0@s.whatsapp.net', 'Set Prefix Bot')
 }
 handler.help = ['setprefix'].map(v => v + ' [prefix]')
